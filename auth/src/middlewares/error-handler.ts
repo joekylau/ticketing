c250ffcc1de7f554d3a351c8c7b9cf2089d2c7ab
Joe Lau <joekylau@yahoo.com.hk>
2022-03-13 14:39:06 +0800

    Fix semi colon

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -7,6 +7,10 @@ export const errorHandler = (err: Error, req: Request, res: Response, next: Next
   }
 
   res.status(400).send({
-    errors: [{ message: 'Something went wrong' }];
+    errors: [
+      {
+        message: 'Something went wrong'
+      }
+    ]
   });
 };
